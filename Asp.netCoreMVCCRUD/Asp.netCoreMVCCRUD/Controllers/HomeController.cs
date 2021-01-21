4406ed0539d2509410f5003ee90184d2c8684c50
Hashan Jay` <62950833+hashan-altus@users.noreply.github.com>
2021-01-20 22:09:41 -0500

    Update HomeController.cs

--- Asp.netCoreMVCCRUD/Asp.netCoreMVCCRUD/Controllers/HomeController.cs
+++ Asp.netCoreMVCCRUD/Asp.netCoreMVCCRUD/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace Asp.netCoreMVCCRUD.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
+        private string PrivacyHelp()
+        {
+            return "";
+        }
     }
 }
